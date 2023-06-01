6fe4c7d23a6cb4ac67f11bb204dc66dd3e4cc48a
ChristosTh <92111197+ChristosTh@users.noreply.github.com>
2023-06-01 22:49:57 +0300

    Add files via upload

---
+++ UsingRobotHardware/TeleOpTemplate.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Tutorial;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "TeleOpTemplate")
+public class TeleOpTemplate extends OpMode {
+
+    // Variable Declarations
+
+    @Override
+    public void init() {
+
+        // Init
+
+        // Getting hardware components
+
+    }
+
+    @Override
+    public void loop() {
+
+        // Main Program
+
+    }
+
+}
