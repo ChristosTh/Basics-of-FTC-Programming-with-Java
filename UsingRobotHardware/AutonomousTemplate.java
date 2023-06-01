4d0126cd75e8569de2d793579f0851ca2264414a
ChristosTh <92111197+ChristosTh@users.noreply.github.com>
2023-06-01 22:53:32 +0300

    Add files via upload

---
+++ UsingRobotHardware/AutonomousTemplate.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.Tutorial;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "AutonomousTemplate")
+public class AutonomousTemplate extends LinearOpMode {
+
+    // Variable Declarations
+
+    public void runOpMode() {
+
+        // Init
+
+        // Getting hardware components
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            // Loop for the autonomous 30 seconds
+
+        }
+
+    }
+}
